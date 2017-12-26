283e981ef26e423f1f3ffcd7be92f7db5863d2af
Mohammed Jamal <mohdj21@gmail.com>
2017-12-26 18:58:02 +0400

    com

--- ilm_revision_util.py
+++ ilm_revision_util.py
@@ -10,23 +10,21 @@ def getIlmRevisionLog():
     #%matplotlib inline
 
     simplenote = sn.Simplenote('mohdjamal8453@gmail.com', 'simple123')
-    taskNoteKey="41d06e8ced6c42389127e0d727974230"
-    ilmNoteKey="5f7cb643ec884642b250954ff3996f8c"
+    ilm_note_key="5f7cb643ec884642b250954ff3996f8c"
     ilmRevLogKey="9ad07e0dc9e84909b17795e101683c68"
-    monthlyGoalNoteKey="ba7047bd84204ab49e9e271a5b164675"
 
     maula_vaaz_identifier=['maula','ashara']
     # [0,1,2,5,10,21,52,88,140] earlier
-    revsionSchedule=[1,5,21,52,88,140]+[(140+(x+1)*52) for x in range(5)] # assign for next 5 years as well
-    revsionSchedule=[x*7 for x in revsionSchedule]
+    revsion_schedule=[1,5,21,52,88,140]+[(140+(x+1)*52) for x in range(5)] # assign for next 5 years as well
+    revsion_schedule=[x*7 for x in revsion_schedule]
 
     # [0,1,2,5,10,21,52] earlier
-    revsionSchedule_MaulaVaaz=[1,5,12,21,52]+[(52+(x+1)*30) for x in range(10)] # revise maula tus bayaan every 7 months all
-    revsionSchedule_MaulaVaaz=[x*7 for x in revsionSchedule_MaulaVaaz]
+    revsion_schedule_for_maula_vaaz=[1,5,12,21,52]+[(52+(x+1)*30) for x in range(10)] # revise maula tus bayaan every 7 months all
+    revsion_schedule_for_maula_vaaz=[x*7 for x in revsion_schedule_for_maula_vaaz]
 
-    ilmNoteRaw=simplenote.get_note(ilmNoteKey)
+    ilm_note_raw=simplenote.get_note(ilm_note_key)
 
-    ilmNote=ilmNoteRaw[0]['content'].splitlines()[1:]
+    ilmNote=ilm_note_raw[0]['content'].splitlines()[1:]
     ilmNote=[ln.replace("*","").strip() for ln in ilmNote]
     ilmcsv="\n".join(ilmNote)
     datilm=pd.read_csv(StringIO(ilmcsv))
@@ -34,16 +32,16 @@ def getIlmRevisionLog():
     datilm['date']=pd.to_datetime(datilm['date'],format="%d/%m/%y")
     datilm['weekstartdate']=datilm['date'].dt.to_period('W').apply(lambda r: r.start_time)-dt.timedelta(days=1)
 
-    curWeekStartDate=dt.datetime.now() - dt.timedelta(days=dt.datetime.now().weekday()+1)
-    datilm['daysDifference']=datilm.weekstartdate.apply(lambda ilmdate, curWeekStartDate: (curWeekStartDate-ilmdate).days,args=[curWeekStartDate])
+    cur_week_start_date=dt.datetime.now() - dt.timedelta(days=dt.datetime.now().weekday()+1)
+    datilm['daysDifference']=datilm.weekstartdate.apply(lambda ilmdate, curWeekStartDate: (curWeekStartDate-ilmdate).days,args=[cur_week_start_date])
 
-    isAqaMaulaVaaz=datilm.name.apply(lambda x: any([y in x.lower() for y in maula_vaaz_identifier]))
-    datilm_maula=datilm.loc[isAqaMaulaVaaz,:]
-    datilm_other=datilm.loc[~isAqaMaulaVaaz,:]
-    revlog=pd.concat([datilm_maula[datilm_maula.daysDifference.isin(revsionSchedule_MaulaVaaz)],datilm_other[datilm_other.daysDifference.isin(revsionSchedule)]])
+    is_aqa_maula_vaaz=datilm.name.apply(lambda x: any([y in x.lower() for y in maula_vaaz_identifier]))
+    datilm_maula=datilm.loc[is_aqa_maula_vaaz,:]
+    datilm_other=datilm.loc[~is_aqa_maula_vaaz,:]
+    revlog=pd.concat([datilm_maula[datilm_maula.daysDifference.isin(revsion_schedule_for_maula_vaaz)],datilm_other[datilm_other.daysDifference.isin(revsion_schedule)]])
     revlog=revlog[['name','maturity']]
     #revlog=datilm.loc[datilm.daysDifference.isin(revsionSchedule),['name','maturity']]
-    revlog['date']=dt.datetime.strftime(curWeekStartDate,format="%d/%m/%y")
+    revlog['date']=dt.datetime.strftime(cur_week_start_date,format="%d/%m/%y")
     revlog['rating']="na"
     revlog['timetaken']="na"
     revlog=revlog[['date','name','maturity','rating','timetaken']]
--- task_monitor_util.py
+++ task_monitor_util.py
@@ -8,26 +8,62 @@ import datetime as dt
 import my_util
 import matplotlib.pyplot as plt
 import ast
-from IPython.core.display import display, HTML
-
+from IPython.core.display import display, HTML, display_javascript, Javascript
+from togglwrapper import Toggl
 simplenote = sn.Simplenote('mohdjamal8453@gmail.com', 'simple123')
-taskNoteKey = "41d06e8ced6c42389127e0d727974230"
-ilmNoteKey = "5f7cb643ec884642b250954ff3996f8c"
-ilmRevLogKey = "9ad07e0dc9e84909b17795e101683c68"
-monthlyGoalNoteKey = "ba7047bd84204ab49e9e271a5b164675"
 
 
-def get_tasks(reference_date):
-    dat = simplenote.get_note(taskNoteKey)
+def get_tasks_from_toggl(reference_date):
+    toggl_api_token = '188b4b533d58101a5fcc800f9f00569d'
+    toggl_default_workspace_id = 57456
+    number_of_days_of_past_toggl_data_to_pull = 60
+    toggl = Toggl(toggl_api_token)
+
+    # Pull Toggl Data
+    start_date = str(reference_date.date() - dt.timedelta(days=number_of_days_of_past_toggl_data_to_pull)) + "T00:00:00+00:00"
+    end_date = str(reference_date.date() + dt.timedelta(days=1)) + "T00:00:00+00:00"
+
+    # Pull projects
+    projects = pd.DataFrame(data=toggl.Workspaces.get_projects(toggl_default_workspace_id))
+    projects = projects.loc[:, ['id', 'name']]
+
+    # Pull time entries
+    time_entries = toggl.TimeEntries.get(start_date=start_date, end_date=end_date)
+    if len(time_entries) == 0:
+        return pd.DataFrame()
+    time_entries = pd.DataFrame(data=time_entries)
+    time_entries.stop = pd.to_datetime(time_entries.stop)
+    time_entries.start = pd.to_datetime(time_entries.start)
+    time_entries = time_entries.loc[:, ['start', 'description', 'duration', 'pid']]
+    tasks = pd.merge(time_entries, projects, how='left', left_on='pid', right_on='id')
+    tasks = tasks.loc[:, ['start', 'name', 'description', 'duration']]
+
+    tasks['Date'] = tasks.start.apply(lambda x: dt.datetime(x.year, x.month, x.day))
+    tasks['EndTime'] = tasks.start.apply(lambda x: dt.datetime.strftime(x, "%H:%M"))
+    tasks['Point'] = 1
+    tasks.rename({'name': 'Category', 'description': 'Desc', 'duration': 'Duration'}, inplace=True, axis=1)
+    tasks.Category.fillna(value="", inplace=True)
+    tasks.Category = tasks.Category.apply(lambda x: x.strip().lower())  # remove whitestrips
+    tasks['DateStr'] = tasks.Date.apply(lambda x: dt.datetime.strftime(x, format="%b-%d"))
+    tasks['Duration'] = pd.to_numeric(tasks.Duration, errors="coerce") / 60  # coerce to numeric if any blank durations
+    tasks['Duration'] = tasks['Duration'].apply(lambda x: round(x))
+    tasks.drop(labels=["start"], axis=1, inplace=True)
+    tasks = tasks.loc[:, ['Date', 'EndTime', 'Category', 'Desc', 'Duration', 'Point', 'DateStr']]
+    return tasks
+
+
+def get_tasks_from_simplenote(reference_date):
+    task_note_key = "41d06e8ced6c42389127e0d727974230"
+    dat = simplenote.get_note(task_note_key)
     tasks = ""
-    curdate = ""
+    cur_date = ""
     for ln in dat[0]['content'].splitlines():
         if '*' not in ln and len(ln.strip()) > 0:
-            curdate = ln.strip() + ' ' + str(reference_date.year)
+            cur_date = ln.strip() + ' ' + str(reference_date.year)
         elif '*' in ln:
             if ln.count(",") == 3:
                 ln = ln + ",1"
-            tasks = tasks + "\n" + curdate + "," + ln
+            tasks = tasks + "\n" + cur_date + "," + ln
     tasks = tasks.splitlines()
     tasks = [ln.replace("*", "") for ln in tasks if len(ln.split(",")) == 6]
     tasks = pd.read_csv(StringIO('\n'.join(tasks)), header=None,
@@ -39,8 +75,34 @@ def get_tasks(reference_date):
     return tasks
 
 
+def get_tasks(reference_date):
+    tasks_from_simplenote=get_tasks_from_simplenote(reference_date)
+    tasks_from_toggl=get_tasks_from_toggl(reference_date)
+    tasks=pd.concat([tasks_from_simplenote,tasks_from_toggl],axis=0)
+    tasks.reset_index(inplace=True,drop=True)
+    return tasks
+
+
+def get_task_tracker_config():
+    task_tracker_config_note_key = '58cb45a3-54ac-4316-9a57-7272b8edc0a3'
+    task_tracker_config_note_raw_content = simplenote.get_note(task_tracker_config_note_key)[0]['content']
+    task_tracker_config={}
+    for code_str in task_tracker_config_note_raw_content.splitlines():
+        code_str=code_str.replace("*","").strip()
+        if(code_str.count("=")==0):
+            continue
+        if(code_str.count("#")==0):
+            code_str=code_str[:code_str.index("#")] # remove comments
+        equalto_index=code_str.index("=")
+        var_name=code_str[:equalto_index]
+        var_value=eval(code_str[equalto_index+1:])
+        task_tracker_config[var_name]=var_value
+    return task_tracker_config
+
+
 def __get_goal_details(reference_date):
-    rawGoal = simplenote.get_note(monthlyGoalNoteKey)
+    monthly_goal_note_key = "ba7047bd84204ab49e9e271a5b164675"
+    rawGoal = simplenote.get_note(monthly_goal_note_key)
     rawGoal = rawGoal[0]['content'].splitlines()
     rawGoal = [ln for ln in rawGoal if ln.strip() != ""]
 
@@ -65,8 +127,8 @@ def __get_goal_details(reference_date):
     return goal, goal_config
 
 
-def is_task_within_goal(task_dates, goal_config):
-    def isWithinGoal_func(date, startDate, endDate, holidays=[], excludeWeekend=True):
+def get_tasks_towards_current_goal(tasks, goal, goal_config):
+    def is_task_within_goal_date_func(date, startDate, endDate, holidays=[], excludeWeekend=True):
         endDate = endDate - dt.timedelta(days=1)  # increasing date by 1 for easier comparison
         # check if date within start and enddate, if not in holidays and if not a weekend
         if (excludeWeekend):
@@ -75,9 +137,12 @@ def is_task_within_goal(task_dates, goal_config):
             out = (date >= startDate) & (date < endDate) & (date not in holidays)
         return out
 
-    is_within_goal = task_dates.apply(isWithinGoal_func,
+    is_within_goal = tasks.Date.apply(is_task_within_goal_date_func,
                                       args=[goal_config['From'], goal_config['To'], goal_config['holidays']])
-    return is_within_goal
+    goal_category = [x.strip() for x in goal.short_name.unique()]
+
+    tasks_in_goal = tasks[is_within_goal & (tasks.Category.isin(goal_category))]
+    return tasks_in_goal
 
 
 def get_goal_along_with_all_metrics(tasks, reference_date):
@@ -85,8 +150,7 @@ def get_goal_along_with_all_metrics(tasks, reference_date):
     goal, goal_config = __get_goal_details(reference_date)
 
     # isWithinGoal = is_task_within_goal(tasks['Date'],goal_config)
-    goal_category = [x.strip() for x in goal.short_name.unique()]
-    tasks_in_goal = tasks[(is_task_within_goal(tasks['Date'], goal_config)) & (tasks.Category.isin(goal_category))]
+    tasks_in_goal = get_tasks_towards_current_goal(tasks, goal, goal_config)
     duration_by_category = tasks_in_goal.groupby('Category', as_index=False)['Duration'].sum().rename(
         columns={'Duration': 'hours_completed'})
     point_by_category = tasks_in_goal.groupby('Category', as_index=False)['Point'].mean().rename(
@@ -111,10 +175,17 @@ def get_goal_along_with_all_metrics(tasks, reference_date):
     return goal, goal_config
 
 
+def __is_current_week(date_to_compare,reference_date,exclude_weekend=False,holidays=[]):
+    current_week_number = int(reference_date.strftime('%W'))
+    if exclude_weekend:
+        is_current_week = (int((date_to_compare + dt.timedelta(days=1)).strftime('%W')) == current_week_number) & (date_to_compare.weekday() not in [4, 5]) & (date_to_compare not in holidays)
+    else:
+        is_current_week = (int((date_to_compare + dt.timedelta(days=1)).strftime('%W')) == current_week_number) & (date_to_compare not in holidays)
+    return is_current_week
+
+
 def determine_n_display_goal_metrics(tasks,tasks_in_goal,goal,goal_config,reference_date):
-    currentWeek = int(reference_date.strftime('%W'))
-    isCurrentWeek = tasks.Date.apply(
-        lambda x, currentWeek: int((x + dt.timedelta(days=1)).strftime('%W')) == currentWeek, args=[currentWeek])
+    is_current_week = tasks.Date.apply(__is_current_week, args=[reference_date])
 
     dates_in_goal_period = [x for x in pd.date_range(start=goal_config['From'], end=goal_config['To'])]
     total_days = len([x for x in dates_in_goal_period if x.weekday() not in [4, 5]]) - len(goal_config['holidays'])
@@ -130,7 +201,7 @@ def determine_n_display_goal_metrics(tasks,tasks_in_goal,goal,goal_config,refere
     days_from_deadline = total_days - total_days_till_now
 
     # Goal Metrics
-    goal_hrs_in_cur_week = tasks_in_goal.Duration[isCurrentWeek].sum() / 60
+    goal_hrs_in_cur_week = tasks_in_goal.Duration[is_current_week].sum() / 60
     days_remaining_in_cur_week = 5 - (reference_date + dt.timedelta(days=1)).weekday()
     ideal_goal_hrs_till_now_in_week = ((reference_date + dt.timedelta(days=1)).weekday()) * goal_config['hours']
     weekly_goal_hr_lagging = round(ideal_goal_hrs_till_now_in_week - goal_hrs_in_cur_week, 1)
@@ -226,9 +297,37 @@ def calculate_n_display_task_n_goal_metrics(reference_date):
     goal, goal_config = get_goal_along_with_all_metrics(tasks, reference_date)
 
     # Get hours completed against goal from tasks and append to existing goal
-    goal_category = [x.strip() for x in goal.short_name.unique()]
-    tasks_in_goal = tasks[(is_task_within_goal(tasks['Date'], goal_config)) & (tasks.Category.isin(goal_category))]
+    tasks_in_goal = get_tasks_towards_current_goal(tasks, goal, goal_config)
     determine_n_display_focused_hour_metric(tasks, tasks_in_goal, goal_config, reference_date)
     determine_n_display_goal_metrics(tasks, tasks_in_goal, goal, goal_config, reference_date)
     display_goal_status(goal)
 
+
+def get_weekly_hour_spent_goal_status(tasks,tracker_config,reference_date):
+    tasks_in_current_week=tasks.loc[tasks.Date.apply(tk.__is_current_week,args=[reference_date]),:]
+    weekly_goal_current_status=tasks_in_current_week.groupby('Category',as_index=False)['Duration'].sum()
+    weekly_goal_current_status.rename(mapper={'Duration':'Actual'},axis=1,inplace=True)
+    weekly_goal_current_status.Actual=round(weekly_goal_current_status.Actual/60,1)
+    weekly_goal=pd.DataFrame(pd.Series(tracker_config['weekly_goal']),columns=['Ideal']).reset_index(drop=False).rename({'index':'Category'},axis=1)
+    weekly_goal_current_status=pd.merge(weekly_goal,weekly_goal_current_status,how='left',on='Category')
+    weekly_goal_current_status.Actual.fillna(0,inplace=True)
+    return weekly_goal_current_status
+
+
+def get_daily_hour_spent_status(tasks,tracker_config,goal_config,reference_date):
+    daily_goal=pd.DataFrame(pd.Series(tracker_config['daily_goal']),columns=['Ideal']).reset_index(drop=False).rename({'index':'Category'},axis=1)
+
+    tasks_in_current_week_in_company=tasks.loc[tasks.Date.apply(__is_current_week,args=[reference_date,True,goal_config['holidays']]),:]
+    avg_daily_category_wise_distribution_of_tasks_in_company=tasks_in_current_week_in_company.groupby('Category',as_index=False)['Duration'].sum()
+    avg_daily_category_wise_distribution_of_tasks_in_company.rename(mapper={'Duration':'Avg_weekly'},axis=1,inplace=True)
+    avg_daily_category_wise_distribution_of_tasks_in_company.Avg_weekly=round(avg_daily_category_wise_distribution_of_tasks_in_company.Avg_weekly/(5*60),1)
+
+    tasks_today=tasks.loc[tasks.Date.apply(lambda x:x.date()==reference_date.date()),:]
+    today_category_wise_distribution_of_tasks=tasks_today.groupby('Category',as_index=False)['Duration'].sum()
+    today_category_wise_distribution_of_tasks.rename(mapper={'Duration':'Today'},axis=1,inplace=True)
+    today_category_wise_distribution_of_tasks.Today=round(today_category_wise_distribution_of_tasks.Today/(60),1)
+
+    daily_goal_status=pd.merge(daily_goal,avg_daily_category_wise_distribution_of_tasks_in_company,how="left",on="Category")
+    daily_goal_status=pd.merge(daily_goal_status,today_category_wise_distribution_of_tasks,how="left",on="Category")
+    daily_goal_status.fillna(0, inplace=True)
+    return daily_goal_status
